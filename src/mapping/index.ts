8458f116336214a5ac19f123ce9c2e22eab5994c
ardiantoramadhan <56662573+ardianto29@users.noreply.github.com>
2024-09-13 22:26:29 +0700

    feat(mapping): add pics about

---
+++ src/mapping/index.ts
@@ -0,0 +1,42 @@
+import Pic1 from "../../public/images/Alya3.jpg";
+import Pic2 from "../../public/images/Yuki.jpg";
+import Pic3 from "../../public/images/Masha2.jpg";
+// import Pic4 from "../../public/images/kuze.jpg";
+import Pic5 from "../../public/images/Ayano1.jpg";
+
+export const pics = [
+  {
+    src: Pic1,
+    bottom: "-bottom-120",
+    right: "right-[4vw]",
+    origin: "bottom",
+  },
+
+  {
+    src: Pic2,
+    left: "left-[1vw]",
+    bottom: "bottom-10",
+    origin: "left",
+  },
+
+  {
+    src: Pic3,
+    left: "left-[7vw]",
+    top: "-top-[17.8rem]",
+    origin: "top",
+  },
+
+  {
+    src: Pic5,
+    right: "right-0",
+    top: "-top-[15vw]",
+    origin: "right",
+  },
+
+  //   {
+  //     src: Pic4,
+  //     top: "-top-[15vw]",
+  //     right: "right-[4vw]",
+  //     origin: "center",
+  //   },
+];
