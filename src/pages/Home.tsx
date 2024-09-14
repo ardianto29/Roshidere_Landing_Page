1884b4c5775c768e8491a9d07e5ff15976eb7fa5
ardiantoramadhan <56662573+ardianto29@users.noreply.github.com>
2024-09-14 23:07:42 +0700

    style: add overflow x hidden

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,16 @@
 import { About } from "../sections/About";
 import { Character } from "../sections/Character";
+import { Events } from "../sections/Events";
 import { Hero } from "../sections/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <main>
+      <main className="overflow-x-hidden">
         <About />
         <Character />
+        <Events />
       </main>
     </>
   );
